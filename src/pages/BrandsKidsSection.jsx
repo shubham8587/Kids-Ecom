1db8f3a41fc5577ae0fae5944cb93e9a660b40ae
shubham8587 <106688040+shubham8587@users.noreply.github.com>
2026-07-14 19:24:54 +0530

    Fix Error in BradsKidSection

--- src/pages/BrandsKidsSection.jsx
+++ src/pages/BrandsKidsSection.jsx
@@ -1,9 +1,8 @@
-import { Carousel, Container, Row, Col, Button } from "react-bootstrap";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import brand1 from "../assets/images/brand1.webp";
 import brand2 from "../assets/images/brand2.webp";
 
- 
 const offers = [
   {
     id: 1,
@@ -11,46 +10,42 @@ const offers = [
   },
   {
     id: 2,
- image: brand2,
+    image: brand2,
   },
   {
     id: 3,
-     image: brand1,
+    image: brand1,
   },
   {
     id: 4,
-   image: brand2,
+    image: brand2,
   },
-    {
+  {
     id: 5,
-   image: brand1,
+    image: brand1,
   },
-    {
+  {
     id: 6,
- image: brand2,
+    image: brand2,
   },
-  
 ];
 
 function BrandsKidsSection() {
   return (
     <section className="offer-slider-section py-5">
       <Container>
+        <div className="collection-header">
+          <div className="floating-circle circle1"></div>
+          <div className="floating-circle circle2"></div>
+          <div className="floating-circle circle3"></div>
 
-                        <div className="collection-header">
-  <div className="floating-circle circle1"></div>
-  <div className="floating-circle circle2"></div>
-  <div className="floating-circle circle3"></div>
+          <div className="floating-star star1">⭐</div>
+          <div className="floating-star star2">🌈</div>
+          <div className="floating-star star3">🎈</div>
 
-  <div className="floating-star star1">⭐</div>
-  <div className="floating-star star2">🌈</div>
-  <div className="floating-star star3">🎈</div>
-
-  <h2>   Brands You Love</h2>
-  <button className="view-btn">
-    View All →
-  </button>
-</div> 
+          <h2> Brands You Love</h2>
+          <button className="view-btn">View All →</button>
+        </div>
         <Carousel
           indicators={false}
           interval={3000}
@@ -68,26 +63,17 @@ function BrandsKidsSection() {
           {[0, 2].map((start) => (
             <Carousel.Item key={start}>
               <Row className="g-4">
-
                 {offers.slice(start, start + 3).map((item) => (
                   <Col lg={4} key={item.id}>
                     <div className="offer-card">
-
-                      <img
-                        src={item.image}
-                        alt=""
-                        className="img-fluid"
-                      />
-
+                      <img src={item.image} alt="" className="img-fluid" />
                     </div>
                   </Col>
                 ))}
-
               </Row>
             </Carousel.Item>
           ))}
         </Carousel>
-
       </Container>
     </section>
   );
